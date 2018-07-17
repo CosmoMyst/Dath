29e2f3bed3a7f6a676951f5db97ebc91e2c81519
CodeMyst <codemyst@outlook.com>
2018-07-17 12:50:21 +0200

    Added Vector2 struct

--- source/monomyst.math/vector2.d
+++ source/monomyst.math/vector2.d
@@ -1,9 +1,9 @@
-module monomyst.math.vector3;
+module monomyst.math.vector2;
 
 import core.simd;
 
-/// Vector3 struct
-struct Vector3
+/// Vector2 struct
+struct Vector2
 {
     private float4 v;
 
@@ -17,17 +17,11 @@ struct Vector3
     /// ditto
     @property float y (float value) { return v.array [1] = value; }
 
-    /// The Z  component of a vector
-    @property float z () { return v.array [2]; }
-    /// ditto
-    @property float z (float value) { return v.array [2] = value; }
-
     /// Constructs a Vector3 using X, Y and Z components
-    this (float x, float y, float z)
+    this (float x, float y)
     {
         this.x = x;
         this.y = y;
-        this.z = z;
     }
 
     /// Negates all the components of the vector
@@ -37,7 +31,7 @@ struct Vector3
     }
 
     /// Adds two vectors together
-    void add (const Vector3 b)
+    void add (const Vector2 b)
     {
         v += b.v;
     }
@@ -45,10 +39,10 @@ struct Vector3
 
 unittest
 {
-    Vector3 vector = Vector3 (43, 56, 13);
-    assert (vector.x == 43 && vector.y == 56 && vector.z == 13);
+    Vector2 vector = Vector2 (43, 56);
+    assert (vector.x == 43 && vector.y == 56);
 
     vector.negate ();
-    assert (vector.x == -43 && vector.y == -56 && vector.z == -13);
+    assert (vector.x == -43 && vector.y == -56);
     vector.negate ();
 }
