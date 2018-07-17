a20500fc685e150a4395cba20120e2f326fb7a73
CodeMyst <codemyst@outlook.com>
2018-07-17 11:20:58 +0200

    Basic Vector3 struct

---
+++ source/monomyst.math/vector3.d
@@ -0,0 +1,54 @@
+module monomyst.math;
+
+import core.simd;
+
+/// Vector3 struct
+struct Vector3
+{
+    private float4 v;
+
+    /// The X component of a vector
+    @property float x () { return v.array [0]; }
+    /// ditto
+    @property float x (float value) { return v.array [0] = value; }
+
+    /// The Y component of a vector
+    @property float y () { return v.array [1]; }
+    /// ditto
+    @property float y (float value) { return v.array [1] = value; }
+
+    /// The Z  component of a vector
+    @property float z () { return v.array [2]; }
+    /// ditto
+    @property float z (float value) { return v.array [2] = value; }
+
+    /// Constructs a Vector3 using X, Y and Z components
+    this (float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /// Inverts all the components of the vector
+    void invert ()
+    {
+        v = -v;
+    }
+
+    /// Adds two vectors together
+    void add (const Vector3 b)
+    {
+        v += b.v;
+    }
+}
+
+unittest
+{
+    Vector3 vector = Vector3 (43, 56, 13);
+    assert (vector.x == 43 && vector.y == 56 && vector.z == 13);
+
+    vector.invert ();
+    assert (vector.x == -43 && vector.y == -56 && vector.z == -13);
+    vector.invert ();
+}
