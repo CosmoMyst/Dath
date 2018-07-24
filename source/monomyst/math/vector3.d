5f83a94429c6c8380230d26e95e2172202923c7e
CodeMyst <codemyst@outlook.com>
2018-07-24 20:45:37 +0200

    Added vector3 dot (no SIMD, not supported)

--- source/monomyst/math/vector3.d
+++ source/monomyst/math/vector3.d
@@ -1,11 +1,13 @@
 module monomyst.math.vector3;
 
-import core.simd;
+import inteli.xmmintrin;
+
+version (unittest) import monomyst.math.bassert;
 
 /// Vector3 struct
 struct Vector3
 {
-    private float [4] v;
+    package float [4] v;
 
     /// The X component of a vector
     @property float x () { return v [0]; }
@@ -35,6 +37,20 @@ struct Vector3
         Zero Vector3 (0, 0, 0)
     +/
     static Vector3 zero = Vector3 (0, 0, 0);
+
+    /++
+        Calculates a dot product of two vectors.
+        --------------------
+        (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
+        --------------------
+    +/
+    static float dot (Vector3 a, Vector3 b)
+    {
+        // TODO: Package intel-intrinsics doesn't support this yet since this is SSE4.1
+        // return _mm_cvtss_f32 (_mm_dp_ps (a.v, b.v, 0x71));
+
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
 }
 
 unittest
@@ -45,4 +61,12 @@ unittest
 
     vector = Vector3 (43, 56, 13);
     assert (vector.x == 43 && vector.y == 56 && vector.z == 13);
+}
+
+unittest
+{
+    Vector3 a = Vector3 (2, 5, 3);
+    Vector3 b = Vector3 (7, 4, 9);
+    immutable float res = Vector3.dot (a, b);
+    bassert (res, 61);
 }
