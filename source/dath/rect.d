1cc3b14ef800722f9faaa83ae080d63dac9edf03
CodeMyst <code@myst.rs>
2021-12-27 20:56:53 +0100

    templated Rect, intersection check

--- source/dath/rect.d
+++ source/dath/rect.d
@@ -1,21 +1,34 @@
 module dath.rect;
 
+public alias Rect = RectImpl!float;
+public alias RectI = RectImpl!int;
+
 /++
  + rectangle definition, origin at upper left.
  +/
-struct Rect {
-    float x;
-    float y;
-    float w;
-    float h;
+struct RectImpl(T) {
+    T x;
+    T y;
+    T w;
+    T h;
 }
 
-/++
- + rectangle definition, origin at upper left. uses integers.
- +/
-struct RectI {
-    int x;
-    int y;
-    int w;
-    int h;
+bool intersects(T)(RectImpl!T a, RectImpl!T b) @nogc
+{
+    if (a.x < b.x + b.w &&
+        a.x + a.w > b.x &&
+        a.y < b.y + b.h &&
+        a.y + a.h > b.y)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+unittest
+{
+    Rect a = Rect(0, 0, 10, 10);
+    Rect b = Rect(5, 5, 10, 10);
+    assert(intersects(a, b));
 }
