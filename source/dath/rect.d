a33e4f243d750c631277d8b8a5b9a82b8971b62f
CodeMyst <code@myst.rs>
2022-03-06 14:23:18 +0100

    rect constructors, using vector for pos and size

--- source/dath/rect.d
+++ source/dath/rect.d
@@ -1,6 +1,9 @@
 module dath.rect;
 
+version(unittest) import fluent.asserts;
+
 import std.traits;
+import dath.vector;
 
 public alias Rectf = Rect!float;
 public alias Rectd = Rect!double;
@@ -12,10 +15,57 @@ public alias Rectu = Rect!uint;
  +/
 public struct Rect(T) if (isNumeric!T)
 {
-    T x;
-    T y;
-    T w;
-    T h;
+    union
+    {
+        T[4] v;
+
+        struct
+        {
+            T x;
+            T y;
+            T w;
+            T h;
+        }
+
+        struct
+        {
+            Vec!(T, 2) position;
+            Vec!(T, 2) size;
+        }
+    }
+
+    public this(T value) @nogc pure nothrow
+    {
+        v[] = value;
+    }
+
+    public this(T x, T y, T w, T h) @nogc pure nothrow
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public this(T x, T y, Vec!(T, 2) size) @nogc pure nothrow
+    {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public this (Vec!(T, 2) position, T w, T h) @nogc pure nothrow
+    {
+        this.position = position;
+        this.w = w;
+        this.h = h;
+    }
+
+    public this (Vec!(T, 2) position, Vec!(T, 2) size) @nogc pure nothrow
+    {
+        this.position = position;
+        this.size = size;
+    }
 }
 
 /++ 
@@ -37,7 +87,12 @@ public bool intersects(T)(const Rect!T a, const Rect!T b) @nogc pure nothrow
 @("Rect creation and intersection")
 unittest
 {
-    Rectf a = Rectf(0, 0, 10, 10);
-    Rectf b = Rectf(5, 5, 10, 10);
-    assert(intersects(a, b));
+    const a = Rectf(0, 0, 10, 10);
+    const b = Rectf(5, 5, 10, 10);
+
+    intersects(a, b).should.equal(true);
+
+    const r1 = Rectf(Vec2(10, 10), Vec2(5, 5));
+    
+    r1.should.equal(Rectf(10, 10, 5, 5));
 }
